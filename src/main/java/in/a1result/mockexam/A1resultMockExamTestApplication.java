6d5b9800a9fda7cee3247b63309c122a81593c48
arvind kumar <ikalilinux850@gmail.com>
2025-04-26 09:09:32 +0530

    first commit of application

---
+++ src/main/java/in/a1result/mockexam/A1resultMockExamTestApplication.java
@@ -0,0 +1,15 @@
+package in.a1result.mockexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A1resultMockExamTestApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(A1resultMockExamTestApplication.class, args);
+		System.out.println("started...");
+	}
+
+}
---
+++ src/test/java/in/a1result/mockexam/A1resultMockExamTestApplicationTests.java
@@ -0,0 +1,13 @@
+package in.a1result.mockexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A1resultMockExamTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
